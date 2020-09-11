50e7dc25abe441ac98f5b4b3814a03a49bf93afb
Chamty <ak.cha07.mt@gmail.com>
2020-09-11 17:37:03 +0900

    0911_1

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -41,6 +41,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('user.index') }}">登録情報</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                     document.getElementById('logout-form').submit();">
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,6 +4,7 @@
 
     <h1 id="song-title">登録情報</h1>
     <div class="profile-list">
+    <hr>
       <div class="profile-group">
         <div class="profile-title">名前</div>
         <div class="profile-element">{{ $auth->name }}</div>
@@ -12,6 +13,7 @@
         <div class="profile-title">メールアドレス</div>
         <div class="profile-element">{{ $auth->email }}</div>
       </div>
+    <hr>
     </div>
     <div>
       <a href={{ route('songs.list') }}>戻る</a>
